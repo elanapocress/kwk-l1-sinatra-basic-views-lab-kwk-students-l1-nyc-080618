3d6143bd48e26b2968e61873f94cc6a3c676414d
elanapocress <pocresse@gmail.com>
2018-08-14 19:35:51 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
+ get '/' do
+   erb :index
+ end
+ 
+ get '/elana' do
+   erb :elana
+ end
+ 
+ get '/mallory' do
+   erb :mallory
+ end 
+ 
 
 end
+
+
+
